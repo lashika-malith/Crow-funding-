c9002e4c5d7cd1b905cedecf0ca033a9b45d908f
Lashika Malith <malith.lashika@gmail.com>
2021-08-16 22:25:52 +0530

    secoend section complete

--- crowfunding-page/src/Secsecion.js
+++ crowfunding-page/src/Secsecion.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import LinearProgress from '@material-ui/core/LinearProgress';
+import Divider from '@material-ui/core/Divider';
 
 const useStyles = makeStyles({
   root: {
@@ -8,44 +9,79 @@ const useStyles = makeStyles({
   },
   secsction:{
       display: 'flex', 
-      flexDirection: 'column'
-  },
-  Topsection:{
+      flexDirection: 'column',
+    },
+    Topsection:{
       display: 'flex',
-        marginBottom: '50px'
+      marginBottom: '50px',
+      alignItems: 'center', 
+      '@media (max-width: 600px)':{
+              flexDirection: 'column',
+              textAlign: 'center'
+      }
   },
   States:{
-     
-      '& >  h1':{
+      '& >  h2':{
            fontWeight: '700'
       },
 
       '& > p':{
-          fontWeight: '400',
-          color: 'darkgray'
+        fontWeight: '400',
+        color: 'darkgray'
       },
-
-        '&:nth-of-type(1)':{
-            marginRight: '40px',
-
+      
+      '&:nth-of-type(1)':{
+        marginRight: '40px',
+        
+        '@media (max-width: 600px)':{
+          margin: '0'
+        }
         },
 
       '&:nth-of-type(2)':{
-            paddingRight: '40px',
-            paddingLeft: '40px',
-            borderRight: '1px solid black',
-             borderLeft: '1px solid black'
+            paddingRight: '50px',
+            paddingLeft: '50px',
       },
 
         '&:nth-of-type(3)':{
-           
-            marginLeft: '40px',
+           '@media (max-width: 600px)':{
+          margin: '0'
+        },
+        
+        marginLeft: '40px',
       }
 
-  }
+  },
+
+  bar:{
+    height: '15px',
+    borderRadius: '50px',
+    marginBottom: '40px',
+    marginTop: '-10px',
+    backgroundColor: '  hsl(0, 0%, 98%) !important'
+  },
+
+
+  divders:{
+    height: '65px !important',
+    width: '2px',  
+    margin: '0',
+    marginTop: '-15px',
+    '@media (max-width: 600px)':{
+        height: '2px !important',
+          width: '120px',  
+          marginTop:'15px',
+          marginBottom: '15px'
+        }
+}
 });
+
+
+
+
+
 export default function Secsecion() {
-     const classes = useStyles();
+  const classes = useStyles();
   const [progress, setProgress] = React.useState(0);
 
   React.useEffect(() => {
@@ -63,31 +99,38 @@ export default function Secsecion() {
       clearInterval(timer);
     };
   }, []);
+
+
+
+
     return (
         <div className={classes.secsction}>
             <div className={classes.Topsection}>
 
                 <span className={classes.States}>
-                    <h1>$89,914</h1>
+                    <h2>$89,914</h2>
                     <p>of $100,000 backed</p>
                 </span>
 
+                <hr className={classes.divders}></hr>
+
                 <span className={classes.States}>
-                    <h1>5,007</h1>
+                    <h2>5,007</h2>
                     <p>total backed</p>
                 </span>
 
+                <hr className={classes.divders}></hr>
+               
                 <span className={classes.States}>
-                    <h1>56</h1>
-                    <p>day left</p>
+                    <h2>56</h2>
+                    <p>days left</p>
                 </span>  
 
             </div>
 
             <div>
-            <LinearProgress variant="determinate" value={10}/>
+            <LinearProgress className={classes.bar} variant="determinate" value={progress}/>
             </div>
-
         </div>
     )
 }
